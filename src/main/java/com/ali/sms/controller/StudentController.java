03507ddb5e86f2f4530882b71f58ce0785452984
Ali Habibian <ali.habibian04@gmail.com>
2021-12-27 11:51:07 +0330

    Implement add student feather

--- src/main/java/com/ali/sms/controller/StudentController.java
+++ src/main/java/com/ali/sms/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.ali.sms.controller;
 
+import com.ali.sms.entity.Student;
 import com.ali.sms.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +20,19 @@ public class StudentController {
     public String listStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model){
+        // Create student object to hold student form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
